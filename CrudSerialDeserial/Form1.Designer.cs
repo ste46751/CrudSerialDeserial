c670aeedddcfae51c717947f855df93103517ea4
Quarti.20121 <stefanoquarti060@gmail.com>
2024-09-23 09:32:44 +0200

    tutti i button fino a modifica

--- CrudSerialDeserial/Form1.Designer.cs
+++ CrudSerialDeserial/Form1.Designer.cs
@@ -28,77 +28,118 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
+            this.Btn_Create = new System.Windows.Forms.Button();
+            this.Btn_Read = new System.Windows.Forms.Button();
+            this.Btn_Update = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.txt_nome = new System.Windows.Forms.TextBox();
+            this.txt_cognome = new System.Windows.Forms.TextBox();
+            this.lstbx_lista = new System.Windows.Forms.ListBox();
+            this.txt_ncambia = new System.Windows.Forms.TextBox();
+            this.txt_ccambia = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
-            // button1
+            // Btn_Create
             // 
-            this.button1.Location = new System.Drawing.Point(137, 175);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.Btn_Create.Location = new System.Drawing.Point(103, 142);
+            this.Btn_Create.Margin = new System.Windows.Forms.Padding(2);
+            this.Btn_Create.Name = "Btn_Create";
+            this.Btn_Create.Size = new System.Drawing.Size(56, 19);
+            this.Btn_Create.TabIndex = 0;
+            this.Btn_Create.Text = "Create";
+            this.Btn_Create.UseVisualStyleBackColor = true;
+            this.Btn_Create.Click += new System.EventHandler(this.Btn_Create_Click_1);
             // 
-            // button2
+            // Btn_Read
             // 
-            this.button2.Location = new System.Drawing.Point(218, 175);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
+            this.Btn_Read.Location = new System.Drawing.Point(164, 142);
+            this.Btn_Read.Margin = new System.Windows.Forms.Padding(2);
+            this.Btn_Read.Name = "Btn_Read";
+            this.Btn_Read.Size = new System.Drawing.Size(56, 19);
+            this.Btn_Read.TabIndex = 1;
+            this.Btn_Read.Text = "Read";
+            this.Btn_Read.UseVisualStyleBackColor = true;
+            this.Btn_Read.Click += new System.EventHandler(this.Btn_Read_Click_1);
             // 
-            // button3
+            // Btn_Update
             // 
-            this.button3.Location = new System.Drawing.Point(309, 175);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(75, 23);
-            this.button3.TabIndex = 2;
-            this.button3.Text = "button3";
-            this.button3.UseVisualStyleBackColor = true;
+            this.Btn_Update.Location = new System.Drawing.Point(232, 142);
+            this.Btn_Update.Margin = new System.Windows.Forms.Padding(2);
+            this.Btn_Update.Name = "Btn_Update";
+            this.Btn_Update.Size = new System.Drawing.Size(56, 19);
+            this.Btn_Update.TabIndex = 2;
+            this.Btn_Update.Text = "Update";
+            this.Btn_Update.UseVisualStyleBackColor = true;
+            this.Btn_Update.Click += new System.EventHandler(this.Btn_Update_Click);
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(390, 175);
+            this.button4.Location = new System.Drawing.Point(292, 142);
+            this.button4.Margin = new System.Windows.Forms.Padding(2);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.Size = new System.Drawing.Size(56, 19);
             this.button4.TabIndex = 3;
             this.button4.Text = "button4";
             this.button4.UseVisualStyleBackColor = true;
             // 
-            // textBox1
+            // txt_nome
             // 
-            this.textBox1.Location = new System.Drawing.Point(166, 103);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 22);
-            this.textBox1.TabIndex = 4;
+            this.txt_nome.Location = new System.Drawing.Point(124, 84);
+            this.txt_nome.Margin = new System.Windows.Forms.Padding(2);
+            this.txt_nome.Name = "txt_nome";
+            this.txt_nome.Size = new System.Drawing.Size(76, 20);
+            this.txt_nome.TabIndex = 4;
             // 
-            // textBox2
+            // txt_cognome
             // 
-            this.textBox2.Location = new System.Drawing.Point(334, 103);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 22);
-            this.textBox2.TabIndex = 5;
+            this.txt_cognome.Location = new System.Drawing.Point(250, 84);
+            this.txt_cognome.Margin = new System.Windows.Forms.Padding(2);
+            this.txt_cognome.Name = "txt_cognome";
+            this.txt_cognome.Size = new System.Drawing.Size(76, 20);
+            this.txt_cognome.TabIndex = 5;
+            // 
+            // lstbx_lista
+            // 
+            this.lstbx_lista.FormattingEnabled = true;
+            this.lstbx_lista.Location = new System.Drawing.Point(412, 84);
+            this.lstbx_lista.Name = "lstbx_lista";
+            this.lstbx_lista.Size = new System.Drawing.Size(138, 134);
+            this.lstbx_lista.TabIndex = 6;
+            // 
+            // txt_ncambia
+            // 
+            this.txt_ncambia.Location = new System.Drawing.Point(124, 198);
+            this.txt_ncambia.Margin = new System.Windows.Forms.Padding(2);
+            this.txt_ncambia.Name = "txt_ncambia";
+            this.txt_ncambia.Size = new System.Drawing.Size(76, 20);
+            this.txt_ncambia.TabIndex = 7;
+            // 
+            // txt_ccambia
+            // 
+            this.txt_ccambia.Location = new System.Drawing.Point(250, 198);
+            this.txt_ccambia.Margin = new System.Windows.Forms.Padding(2);
+            this.txt_ccambia.Name = "txt_ccambia";
+            this.txt_ccambia.Size = new System.Drawing.Size(76, 20);
+            this.txt_ccambia.TabIndex = 8;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
+            this.ClientSize = new System.Drawing.Size(600, 366);
+            this.Controls.Add(this.txt_ccambia);
+            this.Controls.Add(this.txt_ncambia);
+            this.Controls.Add(this.lstbx_lista);
+            this.Controls.Add(this.txt_cognome);
+            this.Controls.Add(this.txt_nome);
             this.Controls.Add(this.button4);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Btn_Update);
+            this.Controls.Add(this.Btn_Read);
+            this.Controls.Add(this.Btn_Create);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed_1);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -106,12 +147,15 @@
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button Btn_Create;
+        private System.Windows.Forms.Button Btn_Read;
+        private System.Windows.Forms.Button Btn_Update;
         private System.Windows.Forms.Button button4;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox txt_nome;
+        private System.Windows.Forms.TextBox txt_cognome;
+        private System.Windows.Forms.ListBox lstbx_lista;
+        private System.Windows.Forms.TextBox txt_ncambia;
+        private System.Windows.Forms.TextBox txt_ccambia;
     }
 }
 
--- CrudSerialDeserial/Form1.cs
+++ CrudSerialDeserial/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +13,90 @@ namespace CrudSerialDeserial
 {
     public partial class Form1 : Form
     {
+        List<Person> lista;
         public Form1()
         {
             InitializeComponent();
+            //Creazione della lista all'avvio
+            lista = new List<Person>();
+        }
+
+        class Person
+        {
+            public string _nome { get; set; }
+            public string _cognome { get; set; }
+            public Person(string nome, string cognome)
+            {
+                this._nome = nome;
+                this._cognome = cognome;
+            }
+            public Person()
+            {
+
+            }
+            public string Serializzazione()
+            {
+                string s = $"{_nome};{_cognome}";
+                return s;
+
+            }
+
+            public void Scrivi(List<Person> lista)
+            {
+                StreamWriter sw = new StreamWriter("ListaE.txt");
+
+                for (int i = 0; i < lista.Count; i++)
+                {
+                    sw.WriteLine($"{lista[i]._nome};{lista[i]._cognome}");
+                }
+                sw.Close();
+            }
+        }
+
+        private void Btn_Create_Click_1(object sender, EventArgs e)
+        {
+            string n = txt_nome.Text;
+            string c = txt_cognome.Text;
+
+            //Person person = new Person(n,c);
+
+            lista.Add(new Person(n, c));
+        }
+
+        private void Form1_FormClosed_1(object sender, FormClosedEventArgs e)
+        {
+            Person person = new Person();
+            person.Scrivi(lista);
+        }
+
+        private void Btn_Read_Click_1(object sender, EventArgs e)
+        {
+            for (int i = 0; i < lista.Count; i++)
+            {
+                lstbx_lista.Items.Add($"{lista[i]._nome};{lista[i]._cognome}");
+            }
+        }
+
+        private void Btn_Update_Click(object sender, EventArgs e)
+        {
+            string n = txt_nome.Text;
+            string c = txt_cognome.Text;
+
+            string n1 = txt_ncambia.Text;
+            string c1 = txt_ccambia.Text;
+
+            for (int i = 0;i < lista.Count;i++)
+            {
+                if (n == lista[i]._nome && c == lista[i]._cognome)
+                {
+                    lista[i]._nome=n1;
+                    lista[i]._cognome=c1;
+                }
+                else
+                {
+
+                }
+            }
         }
     }
 }
